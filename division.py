464e02b9ca93d284fa101c2a293080b60a179ebb
jmwania <josphatmwania237@gmail.com>
2018-08-17 04:03:06 +0300

    divide by zero

---
+++ division.py
@@ -0,0 +1,6 @@
+def division(a,b):
+    if b == 0:
+        print("you can't divide by zero")
+    else:
+        result =a/b
+        return result
---
+++ test_division.py
@@ -0,0 +1,16 @@
+import unittest
+from division import division
+
+class testdividion(unittest.TestCase):
+    def test_answer_is_correct(self):
+        self.assertEqual(division(8,4),2)
+
+    def test_answer_works_for_negative(self):
+        self.assertEqual(division(-25,-5),5)
+
+    def test_result_is_not_infinite(self):
+        self.assertRaises(TypeError)
+
+
+if __name__ == "__main__":
+    unittest.main
